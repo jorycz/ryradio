f7408300011f4d70107aa9bdcec9357bd659da81
Josef Rysanek <josef.rysanek@gmail.com>
2020-01-21 20:40:15 +0100

    Radio is not restarted when changing volume.

--- varwwwhtml/scripts/runRadio.sh
+++ varwwwhtml/scripts/runRadio.sh
@@ -2,5 +2,5 @@
 
 killall mpg123 &
 sleep 1
-/sbin/runuser -l www-data -c 'php -f /var/www/html/index.php' 1>/dev/null
+/sbin/runuser -l www-data -c 'php -f /var/www/html/index.php?station' 1>/dev/null
 
