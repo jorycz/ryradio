c86867d16787af487fbd6b02eeca5e4b69902ab4
Josef Rysanek <josef.rysanek@gmail.com>
2021-06-18 13:04:13 +0200

    Night volume script

---
+++ varwwwhtml/scripts/nightDayVolume.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+HOUR="$(date +%H)"
+
+SET_VOL=$(cat /var/www/html/config/_volume)
+DEVICE=$(cat /var/www/html/config/_device)
+LOWER="10"
+
+echo "Current device [ $DEVICE ] and volume [ $SET_VOL ]."
+
+if [ ${HOUR} -ge 8 ] && [ ${HOUR} -le 22 ]
+then
+  amixer set "$DEVICE" "${SET_VOL}"
+else
+  NIGHT_VOLUME="$((SET_VOL-LOWER))"
+  amixer set "$DEVICE" "${NIGHT_VOLUME}"
+fi
+
